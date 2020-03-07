70d1268a822bb8472c3e07936ab18c5909fd2c90
Thomas Husebø <thomas.husebo.90@gmail.com>
2020-03-07 16:37:50 +0100

    #14 BUG FIX: File not Found.
    However, this results in bugs with multiple clients #15. Not completely certain of what happens here. It seems like it tries to read the same file over and over again, causing the figures to never be updated and beeing stuck in loading. Works with only one client.

--- dataCollector/DataCollector.py
+++ dataCollector/DataCollector.py
@@ -11,12 +11,20 @@ def getData(data):
     files = [f for f in listdir(myPath) if isfile(join(myPath, f))]
 
     for file in files:
-        print("Reading file " + file)
-        with open('ignoreDir/measurements/' + file, 'r') as json_file:
-            data.append(json.load(json_file))
-            with open('ignoreDir/completedMeasurements/' + file + '.txt', 'w') as outfile:
-                json.dump(data, outfile)
+        print("\nAttempt reading file " + file)
+        try:
+            with open('ignoreDir/measurements/' + file, 'r') as json_file:
+                data.append(json.load(json_file))
+                with open('ignoreDir/completedMeasurements/' + file + '.txt', 'w') as outfile:
+                    json.dump(data, outfile)
             outfile.close()
-        os.remove(myPath + "\\" + file)
+            os.remove(myPath + "\\" + file)
+            print("...Reading Complete")
+        except (FileNotFoundError, PermissionError):
+            # Then file should've been read by another process
+            # TODO: Assuming this can only happen when processes interfere with each other,
+            # TODO: Add a way to monitor this as it is a symptom of another problem: conflicting processes.
+            print("...Reading failed")
+            continue
 
     return data
