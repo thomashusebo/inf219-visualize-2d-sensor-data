aac82c2a919a98559df63275bc43cd6cf3a109d0
Thomas Husebø <thomas.husebo.90@gmail.com>
2020-03-07 19:07:22 +0100

    #20 Line chart now shows only the last 60 measurements

--- factory/FigureCreator.py
+++ factory/FigureCreator.py
@@ -27,7 +27,7 @@ def getHeatMap(data, iterationID, colorScale):
     return heatmap_fig
 
 
-def getLineChart(data, iterationID, coordinate, color_scale):
+def getLineChart(data, iterationID, coordinate, colorScale):
     if iterationID < 0: return {
         'data': [],
         'layout': go.Layout(title=go.layout.Title(text='No data found'))
@@ -41,18 +41,29 @@ def getLineChart(data, iterationID, coordinate, color_scale):
     keepTrackOfIteration = go.Scatter(x=[iterationID, iterationID],
                                       y=[0, zs[iterationID]],
                                       name="Timestep",
-                                      line=dict(color=color_scale[10][1]),
+                                      line=dict(color=colorScale[10][1]),
                                       )
 
     linechart_data = go.Scatter(x=ts,
                                 y=zs,
                                 name="Resistivity",
-                                line=dict(color=color_scale[0][1])
+                                line=dict(color=colorScale[0][1]),
                                 )
 
-    linechart_layout = go.Layout(
-        title=go.layout.Title(text="Temporal changes in coord:" + str(x + 1) + " " + str(y + 1))
-    )
+    linechart_layout = {
+        'title': "Temporal changes in coord:" + str(x + 1) + " " + str(y + 1),
+        'xaxis': {
+            "side": "bottom",
+            "type": "linear",
+            "range": [
+                max(ts) - 60,
+                max(ts) + 1
+            ],
+        }
+    }
+    #    linechart_layout = go.Layout(
+    #        title=go.layout.Title(text="Temporal changes in coord:" + str(x + 1) + " " + str(y + 1))
+    #    )
 
     linechart_fig = {
         'data': [linechart_data, keepTrackOfIteration],
