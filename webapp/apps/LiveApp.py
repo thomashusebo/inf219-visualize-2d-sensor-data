6f23d23dda5441078a1e74e803bf292ac55515ef
Thomas Husebø <thomas.husebo.90@gmail.com>
2020-06-13 10:04:03 +0200

    #31 Updated html

--- webapp/apps/LiveApp.py
+++ webapp/apps/LiveApp.py
@@ -1,19 +1,75 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_daq as daq
 
 from webapp.apps.AbstractApp import AbstractApp
 from webapp.terminateserver import shutdown_path, shutdown
 
+stylesheet = None
+#stylesheet = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
 
 class LiveApp(AbstractApp):
     def setupOn(self, server):
-        live_app = dash.Dash(__name__, server=server, url_base_pathname=self.url)
+        live_app = dash.Dash(__name__, server=server, url_base_pathname=self.url, external_stylesheets=stylesheet)
         live_app.layout = html.Div([
+            # Page Header
             html.H1('Live View'),
             dcc.Location(id='url', refresh=False),
             html.Div(id="hidden_div"),
             dcc.Link('Shutdown server', href=shutdown_path),
+
+            # Title
+            html.Div([
+                dcc.Markdown('''
+                    ## Reconstruction of SmallTankTest001: Free Flow
+                    #### One new measurement per second, regardless of measurement timestamp
+                    ''')
+            ],
+            ),
+            html.Div([
+                dcc.Markdown('''
+                    ##### Description
+                    For this experiment we filled the tank only tapwater. 
+
+                    ##### Logg
+                    - Added tapwater 
+                        -   then measurements (1-5)
+                    - Added solution tapwater with conditor into (2.5, 1.5)
+                        -   then measurements (6-11)
+                    - Added solution tapwater w/0.23M NaCl, into (2.5, 1.5)
+                        -   then measurements (12-20)
+                    - Added solution tapwater w/0.90M NaCl, into (0.0, 1.5)
+                        -   then measurements (21-27)
+
+
+                    ''')
+            ],
+                className='four columns'
+            ),
+
+            # Time
+            html.Div([
+                html.Div(id='live-clock'),
+                dcc.Interval(
+                    id='interval-component',
+                    interval=1 * 1000,  # milliseconds
+                    n_intervals=0
+                )
+            ]),
+
+            # Heatmap
+            html.Div([
+                dcc.Graph(id='heatmap',
+                          config={
+                              "displaylogo": False,
+                              "modeBarButtonsToRemove": ['zoom2d']
+                          },
+                          ),
+            ],
+                className='three columns'
+            ),
         ])
 
         @live_app.callback(dash.dependencies.Output("hidden_div", "children"),
--- webapp/apps/TemporalApp.py
+++ webapp/apps/TemporalApp.py
@@ -1,19 +1,86 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_daq as daq
 
 from webapp.apps.AbstractApp import AbstractApp
 from webapp.terminateserver import shutdown_path, shutdown
 
+stylesheet = None
+#stylesheet = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
 
 class TemporalApp(AbstractApp):
     def setupOn(self, server):
-        temporal_app = dash.Dash(__name__, server=server, url_base_pathname=self.url)
+        temporal_app = dash.Dash(__name__, server=server, url_base_pathname=self.url, external_stylesheets=stylesheet)
         temporal_app.layout = html.Div([
+            # Page header
             html.H1('Temporal View'),
             dcc.Location(id='url', refresh=False),
             html.Div(id="hidden_div"),
             dcc.Link('Shutdown server', href=shutdown_path),
+
+            # Title
+            html.Div([
+                dcc.Markdown('''
+                   ## Reconstruction of SmallTankTest001: Free Flow
+                   #### One new measurement per second, regardless of measurement timestamp
+                   ''')
+            ],
+            ),
+
+            # Time
+            html.Div([
+                html.Div(id='live-clock'),
+                dcc.Interval(
+                    id='interval-component',
+                    interval=1 * 1000,  # milliseconds
+                    n_intervals=0
+                )
+            ]),
+
+            # Heatmap
+            html.Div([
+                dcc.Graph(id='heatmap',
+                          config={
+                              "displaylogo": False,
+                              "modeBarButtonsToRemove": ['zoom2d']
+                          },
+                          ),
+            ],
+                className='three columns'
+            ),
+
+            # Line chart
+            html.Div([
+                dcc.Graph(id='linechart',
+                          config={
+                              "displaylogo": False,
+                              "modeBarButtonsToRemove": []
+                          }),
+            ],
+                className='four columns'
+            ),
+
+            # Slider
+            html.Div([
+                daq.BooleanSwitch(
+                    id='play-button',
+                    on=True
+                ),
+                daq.Slider(
+                    id='heatmap-slider',
+                    min=1,
+                    value=1,
+                    max=1,
+                    color='black',
+                    handleLabel={"showCurrentValue": True, "label": "Iteration"},
+                    size=0,
+                ),
+
+            ],
+                className='three columns'
+            ),
         ])
 
         @temporal_app.callback(dash.dependencies.Output("hidden_div", "children"),
