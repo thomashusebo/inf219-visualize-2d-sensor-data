003ddbee6b099a354297a0d571567d1de8d56162
Thomas Husebø <thomas.husebo.90@gmail.com>
2020-07-13 15:21:33 +0200

    Bat file now creates directories ignored by git

--- FluidFlower.bat
+++ FluidFlower.bat
@@ -1,2 +1,8 @@
 ECHO Starting FluidFlower...
+ECHO Creating directories ignored by git
+mkdir storage\databases
+mkdir incoming_data
+mkdir mainapp\termination\stop_dir
+mkdir export
+ECHO Starting Application...
 python -m pipenv run python main.py
